c76154951d89e316e5bcd65e6ee6e2c8733d81ab
Jack Du <jack_du@epam.com>
2022-06-12 19:38:21 +0800

    update

--- vars/hello.groovy
+++ vars/hello.groovy
@@ -1,13 +1,3 @@
-package org.cicd
-
-// def getCredentials(def credentialsId, def type) {
-//     if (type == 'Secret text') {
-//         withCredentials([string(credentialsId: credentialsId, variable: 'myCredentials')]) {
-//             return myCredentials
-//         }
-//     }
-// }
-
 def call() {
     return 'Hello world
 }
